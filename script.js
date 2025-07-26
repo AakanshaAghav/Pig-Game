90e3e5009566042ba5d34462889b6967ad4032cb
AakanshaAghav <aakanshaaghav4@gmail.com>
2025-07-27 02:07:36 +0530

    🎯 Completed Pig Game project with full functionality and UI

--- script.js
+++ script.js
@@ -14,16 +14,32 @@ const btnHold = document.querySelector('.btn--hold');
 
 
 // Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.remove('hidden');
+
+    player0El.classList.add('player--active');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player1El.classList.remove('player-active');
+
+};
+
+init();
+
 // Switch to next Player
-
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
 
@@ -32,7 +48,7 @@ const switchPlayer = function () {
 
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
-}
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -70,7 +86,6 @@ btnHold.addEventListener('click', function () {
         if (scores[activePlayer] >= 20) {
             playing = false;
             diceEl.classList.add('hidden');
-            document.getElementById(`name--${activePlayer}`).textContent = `Player ${activePlayer + 1} wins the game!`;
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
             document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
         } else {
@@ -80,15 +95,6 @@ btnHold.addEventListener('click', function () {
     }
 });
 
-btnNew.addEventListener('click',function(){
-    document.querySelector(`.player--0`).classList.add('player--active');
-    document.querySelector(`.player--0`).classList.remove('player--winner');
-    scores = [0, 0];
-    currentScore = 0;
-    activePlayer = 0;
-    score0El.textContent = 0;
-    score1El.textContent = 0;
-    playing = true;
-});
+btnNew.addEventListener('click', init);
 
 
